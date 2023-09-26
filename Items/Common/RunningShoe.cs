99e033d66bc502c0b1753299fd0dc4e3bd4c320f
AquaSG <macvine9@gmail.com>
2023-09-26 15:56:14 -0400

    running shoe

--- Items/Common/RunningShoe.cs
+++ Items/Common/RunningShoe.cs
@@ -11,7 +11,7 @@ namespace TerRoguelike.Items.Common
         public override int itemTier => 0;
         public override void SetDefaults()
         {
-            Item.width = 30;
+            Item.width = 40;
             Item.height = 28;
             Item.rare = ItemRarityID.Blue;
             Item.maxStack = Item.CommonMaxStack;
