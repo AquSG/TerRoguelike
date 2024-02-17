9a668d9b7a0b99b3b27354fd549b3cff4175658c
AquaSG <macvine9@gmail.com>
2024-02-17 18:16:55 -0500

    crimator

--- NPCs/Enemy/Crimator.cs
+++ NPCs/Enemy/Crimator.cs
@@ -20,34 +20,34 @@ using Terraria.DataStructures;
 
 namespace TerRoguelike.NPCs.Enemy
 {
-    public class Corruptor : BaseRoguelikeNPC
+    public class Crimator : BaseRoguelikeNPC
     {
-        public override int modNPCID => ModContent.NPCType<Corruptor>();
-        public override List<int> associatedFloors => new List<int>() { FloorDict["Corrupt"] };
+        public override int modNPCID => ModContent.NPCType<Crimator>();
+        public override List<int> associatedFloors => new List<int>() { FloorDict["Crimson"] };
         public override int CombatStyle => 2;
         public override void SetStaticDefaults()
         {
-            Main.npcFrameCount[modNPCID] = 3;
+            Main.npcFrameCount[modNPCID] = 2;
         }
         public override void SetDefaults()
         {
             base.SetDefaults();
-            NPC.width = 44;
-            NPC.height = 44;
+            NPC.width = 30;
+            NPC.height = 30;
             NPC.aiStyle = -1;
-            NPC.damage = 35;
+            NPC.damage = 30;
             NPC.lifeMax = 600;
             NPC.HitSound = SoundID.NPCHit1;
             NPC.DeathSound = SoundID.NPCDeath1;
             NPC.knockBackResist = 0.5f;
-            modNPC.drawCenter = new Vector2(0, -1);
+            modNPC.drawCenter = new Vector2(0, -2);
             NPC.noGravity = true;
         }
         public override void AI()
         {
-            modNPC.RogueCorruptorAI(NPC, 4f, 0.03f, 30, 120, ModContent.ProjectileType<VileSpit>(), 8f, NPC.damage);
+            modNPC.RogueCorruptorAI(NPC, 4.5f, 0.035f, 30, 135, ModContent.ProjectileType<BloodClot>(), 8f, NPC.damage);
 
-            NPC.frameCounter += 0.2d;
+            NPC.frameCounter += 0.1d;
             float direction = NPC.velocity.ToRotation();
             if (modNPC.targetNPC != -1)
             {
@@ -63,24 +63,20 @@ namespace TerRoguelike.NPCs.Enemy
         {
             if (NPC.life > 0)
             {
-                for (int i = 0; (double)i < hit.Damage / (double)NPC.lifeMax * 60.0; i++)
+                for (int i = 0; (double)i < hit.Damage / (double)NPC.lifeMax * 100.0; i++)
                 {
-                    Dust.NewDust(NPC.position, NPC.width, NPC.height, 18, hit.HitDirection, -1f, NPC.alpha, NPC.color, NPC.scale);
+                    Dust.NewDust(NPC.position, NPC.width, NPC.height, 5, hit.HitDirection, -1f, NPC.alpha);
                 }
             }
             else
             {
                 for (int i = 0; i < 50; i++)
                 {
-                    Dust.NewDust(NPC.position, NPC.width, NPC.height, 18, hit.HitDirection, -2f, NPC.alpha, NPC.color, NPC.scale);
+                    Dust.NewDust(NPC.position, NPC.width, NPC.height, 5, 2 * hit.HitDirection, -2f, NPC.alpha);
                 }
-                Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, 108, NPC.scale);
-                Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, 108, NPC.scale);
-                Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, 109, NPC.scale);
-                Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, 110, NPC.scale);
+                Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, 223);
+                Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, 224);
             }
-            
-
         }
         public override void OnSpawn(IEntitySource source)
         {
