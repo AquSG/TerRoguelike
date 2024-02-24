6454ab2c2d00a154fe99e2f8a0c508d92a2417b4
AquaSG <macvine9@gmail.com>
2024-02-24 10:54:07 -0500

    giant moth

---
+++ NPCs/Enemy/GiantMoth.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using TerRoguelike;
+using TerRoguelike.TerPlayer;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.GameContent;
+using TerRoguelike.NPCs;
+using TerRoguelike.Projectiles;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.Audio;
+using Microsoft.Xna.Framework.Graphics.PackedVector;
+using static TerRoguelike.Schematics.SchematicManager;
+
+namespace TerRoguelike.NPCs.Enemy
+{
+    public class GiantMoth : BaseRoguelikeNPC
+    {
+        public override int modNPCID => ModContent.NPCType<GiantMoth>();
+        public override List<int> associatedFloors => new List<int>() { FloorDict["Jungle"] };
+        public override int CombatStyle => 1;
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[modNPCID] = 3;
+        }
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            NPC.width = 40;
+            NPC.height = 40;
+            NPC.aiStyle = -1;
+            NPC.damage = 24;
+            NPC.lifeMax = 600;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.knockBackResist = 0.4f;
+            modNPC.drawCenter = new Vector2(0, -9);
+            NPC.noGravity = true;
+        }
+        public override void AI()
+        {
+            int attackTelegraph = 30;
+            int attackCooldown = 120;
+            NPC.frameCounter += NPC.ai[2] > 0 ? 0.35d : 0.2d;
+            NPC.rotation = MathHelper.PiOver2 * NPC.velocity.Length() * 0.02f * NPC.direction;
+            modNPC.RogueFlyingShooterAI(NPC, 3.1f, 2f, 0.07f, 96f, 240f, attackTelegraph, attackCooldown, ModContent.ProjectileType<MothDust>(), 2f, Vector2.Zero, NPC.damage, true);
+            if (NPC.ai[2] == -attackCooldown)
+            {
+                SoundEngine.PlaySound(SoundID.Item30 with { Volume = 0.6f }, NPC.Center);
+            }
+        }
+        public override void HitEffect(NPC.HitInfo hit)
+        {
+            if (NPC.life > 0)
+            {
+                for (int i = 0; (double)i < hit.Damage / (double)NPC.lifeMax * 100.0; i++)
+                {
+                    Dust.NewDust(NPC.position, NPC.width, NPC.height, 18, hit.HitDirection, -1f, NPC.alpha, NPC.color, NPC.scale);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    Dust.NewDust(NPC.position, NPC.width, NPC.height, 18, hit.HitDirection, -2f, NPC.alpha, NPC.color, NPC.scale);
+                }
+                Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, 270, NPC.scale);
+                Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, 271, NPC.scale);
+                Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, 271, NPC.scale);
+                Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, 272, NPC.scale);
+            }
+            
+        }
+        public override void FindFrame(int frameHeight)
+        {
+            int currentFrame = ((int)NPC.frameCounter % (1 + Main.npcFrameCount[modNPCID]));
+            if (currentFrame == 3)
+                currentFrame = 1;
+            NPC.frame = new Rectangle(0, currentFrame * frameHeight, TextureAssets.Npc[modNPCID].Value.Width, frameHeight);
+        }
+    }
+}
