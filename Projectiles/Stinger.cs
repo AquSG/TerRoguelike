89698c42a0f15b5c2e3ed3698af1470ee5044240
AquaSG <macvine9@gmail.com>
2024-02-24 19:25:46 -0500

    stinger visual

--- Projectiles/Stinger.cs
+++ Projectiles/Stinger.cs
@@ -11,6 +11,8 @@ using TerRoguelike.TerPlayer;
 using Microsoft.Xna.Framework.Graphics;
 using TerRoguelike.Items.Common;
 using TerRoguelike.Utilities;
+using Terraria.GameContent;
+using Terraria.Graphics.Shaders;
 
 namespace TerRoguelike.Projectiles
 {
@@ -45,6 +47,25 @@ namespace TerRoguelike.Projectiles
                 int d = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, 18, 0f, 0f, 0, default(Color), 1.5f);
                 Main.dust[d].noGravity = true;
             }
+            return true;
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.GameViewMatrix.TransformationMatrix);
+
+            Vector3 colorHSL = Main.rgbToHsl(Color.LimeGreen);
+
+            GameShaders.Misc["TerRoguelike:BasicTint"].UseOpacity(1f);
+            GameShaders.Misc["TerRoguelike:BasicTint"].UseColor(Main.hslToRgb(1 - colorHSL.X, colorHSL.Y, colorHSL.Z));
+            GameShaders.Misc["TerRoguelike:BasicTint"].Apply();
+            for (int i = 0; i < 8; i++)
+            {
+                Vector2 offset = (Vector2.UnitX * 1).RotatedBy(Projectile.rotation + (i * MathHelper.PiOver4));
+                Main.EntitySpriteDraw(TextureAssets.Projectile[Type].Value, Projectile.Center - Main.screenPosition + offset + new Vector2(4, 0), null, Color.White, Projectile.rotation, TextureAssets.Projectile[Type].Value.Size() * 0.5f, Projectile.scale, SpriteEffects.None);
+            }
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.GameViewMatrix.TransformationMatrix);
             return true;
         }
     }
