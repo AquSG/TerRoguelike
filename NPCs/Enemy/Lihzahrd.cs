3539b3e3734355e6d16f58605e299b5d97e5fda9
AquaSG <macvine9@gmail.com>
2024-02-26 14:55:52 -0500

    temple enemies

---
+++ NPCs/Enemy/Lihzahrd.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using TerRoguelike;
+using TerRoguelike.TerPlayer;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.GameContent;
+using TerRoguelike.NPCs;
+using Microsoft.Xna.Framework.Graphics;
+using static TerRoguelike.Schematics.SchematicManager;
+
+namespace TerRoguelike.NPCs.Enemy
+{
+    public class Lihzahrd : BaseRoguelikeNPC
+    {
+        public override int modNPCID => ModContent.NPCType<Lihzahrd>();
+        public override List<int> associatedFloors => new List<int>() { FloorDict["Temple"] };
+        public override int CombatStyle => 0;
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[modNPCID] = 16;
+        }
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            NPC.width = 18;
+            NPC.height = 40;
+            NPC.aiStyle = -1;
+            NPC.damage = 35;
+            NPC.lifeMax = 600;
+            NPC.HitSound = SoundID.NPCHit26;
+            NPC.DeathSound = SoundID.NPCDeath29;
+            NPC.knockBackResist = 0.2f;
+            modNPC.drawCenter = new Vector2(0, -4);
+        }
+        public override void AI()
+        {
+            NPC.frameCounter += NPC.velocity.Length() * 0.25d;
+            modNPC.RogueFighterAI(NPC, 3f, -9.5f, 0.12f);
+            
+        }
+        public override void HitEffect(NPC.HitInfo hit)
+        {
+            NPC.ai[0] = 0;
+            if (NPC.life > 0)
+            {
+                for (int i = 0; (double)i < hit.Damage / (double)NPC.lifeMax * 150.0; i++)
+                {
+                    Dust.NewDust(NPC.position, NPC.width, NPC.height, 5, hit.HitDirection, -1f);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 75; i++)
+                {
+                    Dust.NewDust(NPC.position, NPC.width, NPC.height, 5, 2 * hit.HitDirection, -2f);
+                }
+                Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, 258, NPC.scale);
+                Gore.NewGore(NPC.GetSource_Death(), new Vector2(NPC.position.X, NPC.position.Y + 20f), NPC.velocity, 259, NPC.scale);
+                Gore.NewGore(NPC.GetSource_Death(), new Vector2(NPC.position.X, NPC.position.Y + 20f), NPC.velocity, 259, NPC.scale);
+                Gore.NewGore(NPC.GetSource_Death(), new Vector2(NPC.position.X, NPC.position.Y + 20f), NPC.velocity, 260, NPC.scale);
+                Gore.NewGore(NPC.GetSource_Death(), new Vector2(NPC.position.X, NPC.position.Y + 20f), NPC.velocity, 260, NPC.scale);
+                Gore.NewGore(NPC.GetSource_Death(), new Vector2(NPC.position.X, NPC.position.Y + 20f), NPC.velocity, 261, NPC.scale);
+            }
+        }
+        public override void FindFrame(int frameHeight)
+        {
+            int currentFrame = NPC.velocity.Y == 0 ? (int)(NPC.frameCounter % (Main.npcFrameCount[modNPCID] - 2)) + 2 : 1;
+            NPC.frame = new Rectangle(0, currentFrame * frameHeight, TextureAssets.Npc[modNPCID].Value.Width, frameHeight);
+        }
+    }
+}
