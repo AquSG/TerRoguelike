2030cf907dd3cbcc860af99a28f68658062618e7
AquaSG <macvine9@gmail.com>
2024-03-05 17:09:36 -0500

    wave support

--- Rooms/Forest/ForestEnemyRoom6Up.cs
+++ Rooms/Forest/ForestEnemyRoom6Up.cs
@@ -9,11 +9,13 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using static TerRoguelike.Managers.NPCManager;
+using static TerRoguelike.Schematics.SchematicManager;
 
 namespace TerRoguelike.Rooms
 {
     public class ForestEnemyRoom6Up : Room
     {
+        public override int AssociatedFloor => FloorDict["Forest"];
         public override string Key => "ForestEnemyRoom6Up";
         public override string Filename => "Schematics/RoomSchematics/ForestEnemyRoom6Up.csch";
         public override bool CanExitRight => true;
@@ -21,8 +23,9 @@ namespace TerRoguelike.Rooms
         public override void InitializeRoom()
         {
             base.InitializeRoom();
-            AddRoomNPC(new Vector2(RoomDimensions.X * 8f, RoomDimensions.Y * 8f), ChooseEnemy(2, 0), 60, 120, 0.45f);
-
+            AddRoomNPC(RoomCenter16, ChooseEnemy(AssociatedFloor, 0), 60, 120, 0.45f, 0);
+            AddRoomNPC(RoomCenter16, ChooseEnemy(AssociatedFloor, 1), 60, 120, 0.45f, 1);
+            AddRoomNPC(RoomCenter16, ChooseEnemy(AssociatedFloor, 2), 60, 120, 0.45f, 2);
         }
     }
 }
