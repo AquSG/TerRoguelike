ea03efe07a3f5b0510f58e298461cf309973617e
AquaSG <macvine9@gmail.com>
2024-03-18 17:59:08 -0400

    crimson seer spawning

---
+++ NPCs/Enemy/Boss/CrimsonSeer.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TerRoguelike.Managers;
+using TerRoguelike.Particles;
+using TerRoguelike.Projectiles;
+using TerRoguelike.Systems;
+using static TerRoguelike.Managers.TextureManager;
+using static TerRoguelike.Schematics.SchematicManager;
+using static TerRoguelike.Systems.MusicSystem;
+using static TerRoguelike.Systems.RoomSystem;
+using static TerRoguelike.Utilities.TerRoguelikeUtils;
+
+namespace TerRoguelike.NPCs.Enemy.Boss
+{
+    public class CrimsonSeer : BaseRoguelikeNPC
+    {
+        public override int modNPCID => ModContent.NPCType<CrimsonSeer>();
+        public override List<int> associatedFloors => new List<int>() { FloorDict["Crimson"] };
+        public override int CombatStyle => -1;
+
+        bool ableToHit = true;
+        bool canBeHit = true;
+
+        public int teleportTime = 40;
+        public int teleportMoveTimestamp = 20;
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            NPC.width = 24;
+            NPC.height = 24;
+            NPC.aiStyle = -1;
+            NPC.damage = 35;
+            NPC.lifeMax = 800;
+            NPC.HitSound = SoundID.NPCHit9;
+            NPC.DeathSound = SoundID.NPCDeath11;
+            NPC.knockBackResist = 0f;
+            modNPC.drawCenter = new Vector2(0, 0);
+            modNPC.IgnoreRoomWallCollision = true;
+            NPC.noTileCollide = true;
+            NPC.noGravity = true;
+            modNPC.OverrideIgniteVisual = true;
+        }
+        public override void OnSpawn(IEntitySource source)
+        {
+            NPC.ai[0] = -1;
+            NPC.localAI[1] += Main.rand.Next(50);
+            if (source is EntitySource_Parent parentSource)
+            {
+                if (parentSource.Entity is NPC)
+                {
+                    NPC.ai[0] = parentSource.Entity.whoAmI;
+                    NPC npc = Main.npc[(int)NPC.ai[0]];
+                    if (!npc.active)
+                    {
+                        NPC.StrikeInstantKill();
+                        return;
+                    }
+                        
+                }
+            }
+            if (NPC.ai[0] == -1)
+                NPC.StrikeInstantKill();
+        }
+        public override void AI()
+        {
+            NPC.localAI[1] += Main.rand.NextFloat(0.9f, 1f);
+            NPC.localAI[2]++;
+
+            ableToHit = false;
+            NPC parent = Main.npc[(int)NPC.ai[0]];
+            if (!parent.active)
+                NPC.StrikeInstantKill();
+        }
+        public static void UpdateCrimsonSeer(NPC npc, int seerId, int seerCount)
+        {
+            int spawnTime = 0;
+            //float spawnInterpolant = MathHelper.Clamp(MathHelper.SmoothStep(0, 1f, npc.localAI[2] / spawnTime), 0f, 1f);
+            NPC parent = Main.npc[(int)npc.ai[0]];
+            float interpolant = MathHelper.Clamp((npc.localAI[2] - spawnTime) / 60, 0, 1f);
+            npc.rotation += 0.01f * interpolant;
+            
+            //float magnitude = (136 + (10 * (float)Math.Cos(npc.localAI[1] * 0.05f))) * ((float)Math.Log(spawnInterpolant + 0.01f, 100) + 0.999f);
+            float magnitude = (136 + (10 * (float)Math.Cos(npc.localAI[1] * 0.05f)));
+
+            npc.Center = parent.Center + parent.ModNPC().drawCenter + (npc.rotation.ToRotationVector2() * magnitude);
+        }
+        public override bool CanHitNPC(NPC target)
+        {
+            return ableToHit;
+        }
+        public override bool CanHitPlayer(Player target, ref int cooldownSlot)
+        {
+            return ableToHit;
+        }
+        public override bool? CanBeHitByProjectile(Projectile projectile) => canBeHit ? null : false;
+        public override bool? CanBeHitByItem(Player player, Item item) => canBeHit ? null : false;
+
+        public override void HitEffect(NPC.HitInfo hit)
+        {
+            if (NPC.life > 0)
+            {
+                for (int i = 0; (double)i < hit.Damage / (double)25; i++)
+                {
+                    Dust.NewDust(NPC.position, NPC.width, NPC.height, 5, hit.HitDirection, -1f);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    Dust.NewDust(NPC.position, NPC.width, NPC.height, 5, 2 * hit.HitDirection, -2f);
+                }
+                Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, 402);
+            }
+        }
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            Texture2D tex = TextureAssets.Npc[Type].Value;
+            Vector2 drawPos = NPC.Center + modNPC.drawCenter;
+            Color color = Color.Lerp(Color.White, Lighting.GetColor(drawPos.ToTileCoordinates()), 0.6f);
+            float rotation = 0;
+            Vector2 scale = new Vector2(NPC.scale);
+            if (NPC.ai[3] > 0)
+            {
+                float interpolant = NPC.ai[3] < teleportMoveTimestamp ? NPC.ai[3] / (teleportMoveTimestamp) : 1f - ((NPC.ai[3] - teleportMoveTimestamp) / (teleportTime - teleportMoveTimestamp));
+                float verticInterpolant = MathHelper.Lerp(1f, 2f, 0.5f + (0.5f * -(float)Math.Cos(interpolant * MathHelper.TwoPi)));
+                float horizInterpolant = MathHelper.Lerp(0.5f + (0.5f * (float)Math.Cos(interpolant * MathHelper.TwoPi)), 8f, interpolant * interpolant);
+                scale.X *= horizInterpolant;
+                scale.Y *= verticInterpolant;
+
+                scale *= 1f - interpolant;
+            }
+
+            if (modNPC.ignitedStacks.Any())
+            {
+                spriteBatch.End();
+                spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.GameViewMatrix.TransformationMatrix);
+
+                Color fireColor = Color.Lerp(Color.Yellow, Color.OrangeRed, Main.rand.NextFloat(0.4f, 0.6f + float.Epsilon) + 0.2f + (0.2f * (float)Math.Cos((Main.GlobalTimeWrappedHourly * 20f)))) * 0.8f;
+                Vector3 colorHSL = Main.rgbToHsl(fireColor);
+                float outlineThickness = 1f;
+
+                GameShaders.Misc["TerRoguelike:BasicTint"].UseOpacity(1f);
+                GameShaders.Misc["TerRoguelike:BasicTint"].UseColor(Main.hslToRgb(1 - colorHSL.X, colorHSL.Y, colorHSL.Z));
+                GameShaders.Misc["TerRoguelike:BasicTint"].Apply();
+
+                for (float j = 0; j < 1; j += 0.125f)
+                {
+                    spriteBatch.Draw(tex, drawPos - Main.screenPosition + ((j * MathHelper.TwoPi + NPC.rotation).ToRotationVector2() * outlineThickness), NPC.frame, color, rotation, NPC.frame.Size() * 0.5f, scale, NPC.spriteDirection > 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
+                }
+                spriteBatch.End();
+                spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.GameViewMatrix.TransformationMatrix);
+            }
+            Main.EntitySpriteDraw(tex, drawPos - Main.screenPosition, NPC.frame, color, rotation, NPC.frame.Size() * 0.5f, scale, NPC.spriteDirection > 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
+            
+            return false;
+        }
+    }
+}
