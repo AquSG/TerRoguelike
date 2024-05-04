99de7e99bbe53bdea6520234227c01f9aa9f79e2
AquaSG <macvine9@gmail.com>
2024-05-04 01:10:08 -0400

    WoF spawn and death anim

--- NPCs/Enemy/Boss/WallOfFlesh.cs
+++ NPCs/Enemy/Boss/WallOfFlesh.cs
@@ -48,6 +48,7 @@ namespace TerRoguelike.NPCs.Enemy.Boss
         public Texture2D bodyTex;
         public Texture2D eyeTex;
         public Texture2D mouthTex;
+        public Texture2D godRayTex;
         public SlotId DeathraySlot;
         public float topEyeRotation = MathHelper.Pi;
         public float bottomEyeRotation = MathHelper.Pi;
@@ -56,8 +57,9 @@ namespace TerRoguelike.NPCs.Enemy.Boss
         public double mouthFrameCounter = 0;
 
         public int deadTime = 0;
-        public int cutsceneDuration = 120;
+        public int cutsceneDuration = 240;
         public int deathCutsceneDuration = 180;
+        List<GodRay> deathGodRays = [];
 
         public static Attack None = new Attack(0, 0, 100);
         public static Attack Laser = new Attack(1, 30, 180);
@@ -101,6 +103,7 @@ namespace TerRoguelike.NPCs.Enemy.Boss
             bodyTex = TexDict["WallOfFleshBody"].Value;
             eyeTex = TexDict["WallOfFleshEye"].Value;
             mouthTex = TexDict["WallOfFleshMouth"].Value;
+            godRayTex = TexDict["GodRay"].Value;
         }
         public override void OnSpawn(IEntitySource source)
         {
@@ -117,6 +120,7 @@ namespace TerRoguelike.NPCs.Enemy.Boss
         }
         public override void PostAI()
         {
+            NPC.localAI[1]++;
             if (target != null)
             {
                 bool hellbeam = NPC.ai[0] == Deathray.Id && NPC.ai[1] >= 90;
@@ -229,17 +233,58 @@ namespace TerRoguelike.NPCs.Enemy.Boss
 
                 if (NPC.localAI[0] == -cutsceneDuration)
                 {
-                    CutsceneSystem.SetCutscene(spawnPos, cutsceneDuration, 30, 30, 2.5f);
+                    Vector2 soundPos = NPC.Center + hitboxes[1].offset;
+                    SoundEngine.PlaySound(SoundID.DD2_WitherBeastAuraPulse with { Volume = 1f, Pitch = -1f, MaxInstances = 2 }, soundPos);
+                    SoundEngine.PlaySound(SoundID.DD2_SkeletonSummoned with { Volume = 1f, Pitch = -1f, MaxInstances = 2 }, soundPos);
+                    CutsceneSystem.SetCutscene(NPC.Center + hitboxes[1].offset.X * Vector2.UnitX, cutsceneDuration, 30, 30, 1.25f);
                 }
                 NPC.localAI[0]++;
 
-                if (NPC.localAI[0] == -30)
+                if (NPC.localAI[0] == -cutsceneDuration + 60)
                 {
-                    NPC.immortal = false;
-                    NPC.dontTakeDamage = false;
-                    NPC.ai[1] = -1;
-                    NPC.ai[0] = Summon.Id;
+                    Vector2 soundPos = NPC.Center + hitboxes[1].offset;
+                    SoundEngine.PlaySound(SoundID.DD2_WitherBeastAuraPulse with { Volume = 0.5f, Pitch = -1f, MaxInstances = 2 }, soundPos);
+                    SoundEngine.PlaySound(SoundID.DD2_SkeletonSummoned with { Volume = 0.5f, Pitch = -1f, MaxInstances =2 }, soundPos);
                 }
+                if (NPC.localAI[0] < -30)
+                {
+                    Vector2 basePos = NPC.Center;
+                    basePos = TileCollidePositionInLine(basePos, basePos + new Vector2(500, 0));
+                    basePos -= Vector2.UnitX * 2;
+                    Vector2 topPos = TileCollidePositionInLine(basePos, basePos + new Vector2(0, -1000));
+                    Vector2 bottomPos = TileCollidePositionInLine(basePos, basePos + new Vector2(0, 1000));
+                    float range = topPos.Distance(bottomPos);
+                    int amount = 30;
+                    if (NPC.localAI[0] > -90)
+                    {
+                        amount = (-(int)NPC.localAI[0] - 30) / 2;
+                    }
+                    for (int i = 0; i < amount; i++)
+                    {
+                        Vector2 randPos = topPos + new Vector2(0, Main.rand.NextFloat(range));
+                        Color color = Color.Lerp(Color.Red, Color.DarkRed, Main.rand.NextFloat());
+                        Vector2 vel = Main.rand.NextVector2Circular(2f, 2f);
+                        vel -= Vector2.UnitX * 0.75f;
+                        ParticleManager.AddParticle(new Ball(
+                            randPos, vel,
+                            20, color, new Vector2(0.25f) * Main.rand.NextFloat(0.7f, 1f), 0, 0.96f, 15));
+                    }
+                }
+                if (NPC.localAI[0] >= -60)
+                {
+                    if (NPC.localAI[0] == -60)
+                    {
+                        NPC.ai[1] = -1;
+                        NPC.ai[0] = Summon.Id;
+                    }
+                    else if (NPC.localAI[0] == -30)
+                    {
+                        NPC.immortal = false;
+                        NPC.dontTakeDamage = false;  
+                    }
+                    BossAI();
+                }
+                
             }
             else
             {
@@ -654,9 +699,9 @@ namespace TerRoguelike.NPCs.Enemy.Boss
                     room.bossDead = true;
                     ClearChildren();
                 }
-                CutsceneSystem.SetCutscene(NPC.Center, deathCutsceneDuration, 30, 30, 2.5f);
+                CutsceneSystem.SetCutscene(NPC.Center + hitboxes[1].offset.X * Vector2.UnitX, deathCutsceneDuration, 30, 30, 1.25f);
             }
-            else if (deadTime == 1)
+            else if (deadTime == 1 && modNPC.isRoomNPC)
                 ClearChildren();
 
             void ClearChildren()
@@ -682,9 +727,42 @@ namespace TerRoguelike.NPCs.Enemy.Boss
                     }
                 }
             }
-
             deadTime++;
-            
+
+            if (deadTime % 30 == 0 || deadTime % 52 == 0 || (deadTime > 70 && deadTime % 13 == 0))
+            {
+                SoundEngine.PlaySound(SoundID.NPCHit8 with { Volume = 0.2f, MaxInstances = 4 }, NPC.Center + hitboxes[1].offset);
+                DeathBloodParticles();
+
+                Rectangle bodyRect = hitboxes[0].GetHitbox(NPC.Center, 0);
+                bodyRect.Inflate(0, -100);
+                Vector2 randPos = new Vector2(bodyRect.X, bodyRect.Y + Main.rand.NextFloat(bodyRect.Height));
+                deathGodRays.Add(new GodRay(Main.rand.NextFloat(-MathHelper.PiOver4, MathHelper.PiOver4) + MathHelper.Pi, deadTime, new Vector2(0.16f + Main.rand.NextFloat(-0.02f, 0.02f), 0.018f) * 1.6f, 0, randPos));
+                SoundEngine.PlaySound(SoundID.DD2_MonkStaffGroundImpact with { Volume = 0.35f, Pitch = -0.2f,MaxInstances = 3 }, NPC.Center);
+                SoundEngine.PlaySound(SoundID.NPCHit19 with { Volume = 0.28f, Pitch = -0.6f, PitchVariance = 0.05f,MaxInstances = 3 }, NPC.Center);
+            }
+
+            if (deadTime % 4 == 0)
+            {
+                for (int i = 0; i < deathGodRays.Count; i++)
+                {
+                    GodRay ray = deathGodRays[i];
+                    float rotation = ray.rotation;
+                    Vector2 pos = ray.position;
+                    Vector2 velocity = rotation.ToRotationVector2() * 2.5f;
+                    int xDir = Math.Sign(pos.X - NPC.Center.X);
+                    if (xDir == 0)
+                        xDir = 1;
+                    velocity.X += xDir * 0.6f;
+                    velocity.Y -= 1f;
+                    int time = 40 + Main.rand.Next(20);
+                    Vector2 scale = new Vector2(0.25f, 0.4f) * 0.75f;
+
+                    ParticleManager.AddParticle(new Blood(pos, velocity, time, Color.Black * 0.65f, scale, velocity.ToRotation(), false));
+                    ParticleManager.AddParticle(new Blood(pos, velocity, time, Color.Red * 0.65f, scale, velocity.ToRotation(), true));
+                }
+            }
+
             if (deadTime >= deathCutsceneDuration - 30)
             {
                 NPC.immortal = false;
@@ -693,6 +771,30 @@ namespace TerRoguelike.NPCs.Enemy.Boss
             }
 
             return deadTime >= cutsceneDuration - 30;
+        }
+        public void DeathBloodParticles(bool dead = false)
+        {
+            int time = dead ? 40 : 20;
+            int fadeout = dead ? 35 : 15;
+            float multi = dead ? 0.4f : 1f;
+            float decel = dead ? 0.98f : 0.96f;
+
+            Rectangle particleRect = hitboxes[0].GetHitbox(NPC.Center, 0);
+            particleRect.Inflate(0, -48);
+            for (int j = 0; j < 3; j++)
+            {
+                Vector2 randPos = Main.rand.NextVector2FromRectangle(particleRect);
+                for (int i = 0; i < 16; i++)
+                {
+                    Color color = Color.Lerp(Color.Red, Color.DarkRed, Main.rand.NextFloat());
+                    Vector2 vel = Main.rand.NextVector2Circular(4f, 4f);
+                    vel.Y += 0.5f;
+                    vel *= multi;
+                    ParticleManager.AddParticle(new Ball(
+                        randPos, vel,
+                        time, color, new Vector2(0.25f) * Main.rand.NextFloat(0.7f, 1f), 0, decel, fadeout));
+                }
+            }
         }
         public override void HitEffect(NPC.HitInfo hit)
         {
@@ -706,6 +808,72 @@ namespace TerRoguelike.NPCs.Enemy.Boss
             if (SoundEngine.TryGetActiveSound(DeathraySlot, out var sound) && sound.IsPlaying)
             {
                 sound.Stop();
+            }
+            SoundEngine.PlaySound(SoundID.NPCDeath10 with { Volume = 0.5f }, NPC.Center + hitboxes[1].offset);
+            SoundEngine.PlaySound(SoundID.NPCDeath12 with { Volume = 0.8f, Pitch = -0.5f }, NPC.Center);
+            SoundEngine.PlaySound(SoundID.DD2_KoboldIgnite with { Volume = 0.8f, Pitch = -0.4f }, NPC.Center);
+
+
+            //the following was copied from the vanilla WoF gore code, and I tried cleaning it up a lot. still shows a bit
+            for (int e = 1; e <= 3; e++)
+            {
+                Rectangle rect = hitboxes[e].GetHitbox(NPC.Center, 0);
+                Vector2 pos = new Vector2(rect.X, rect.Y);
+                Dust.NewDust(pos, rect.Width, rect.Height, 5, -2, -1f);
+                if (e > 1)
+                {
+                    Gore.NewGore(NPC.GetSource_Death(), pos, NPC.velocity, 137, NPC.scale);
+                    Gore.NewGore(NPC.GetSource_Death(), new Vector2(pos.X, pos.Y + (rect.Height / 2)), NPC.velocity, 139, NPC.scale);
+                    Gore.NewGore(NPC.GetSource_Death(), new Vector2(pos.X + (rect.Width / 2), pos.Y), NPC.velocity, 139, NPC.scale);
+                    Gore.NewGore(NPC.GetSource_Death(), new Vector2(pos.X + (rect.Width / 2), pos.Y + (rect.Height / 2)), NPC.velocity, 137, NPC.scale);
+                }
+                else
+                {
+                    Gore.NewGore(NPC.GetSource_Death(), new Vector2(pos.X, pos.Y), NPC.velocity, 137, NPC.scale);
+                    Gore.NewGore(NPC.GetSource_Death(), new Vector2(pos.X, pos.Y + (rect.Height / 2)), NPC.velocity, 138, NPC.scale);
+                    Gore.NewGore(NPC.GetSource_Death(), new Vector2(pos.X + (rect.Width / 2), pos.Y), NPC.velocity, 138, NPC.scale);
+                    Gore.NewGore(NPC.GetSource_Death(), new Vector2(pos.X + (rect.Width / 2), pos.Y + (rect.Height / 2)), NPC.velocity, 137, NPC.scale);
+                }
+            }
+
+            Rectangle bodyRect = hitboxes[0].GetHitbox(NPC.Center, 0);
+            Vector2 topLeft = new Vector2(bodyRect.X, bodyRect.Y);
+            for (int x = 0; x < bodyRect.Width - 32; x += 46)
+            {
+                for (int y = 0; y < bodyRect.Height; y += 52)
+                {
+                    Vector2 pos = topLeft + new Vector2(x, y);
+                    if (ParanoidTileRetrieval(pos.ToTileCoordinates()).IsTileSolidGround(true))
+                        continue;
+
+                    if (Main.rand.NextBool(3))
+                        DeathBloodParticles(true);
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Dust.NewDust(pos, 32, 32, 5, Main.rand.NextFloat(-0.6f, 0.6f), Main.rand.NextFloat(-0.6f, 0.6f));
+
+                        if (Main.rand.NextBool())
+                            continue;
+
+                        Vector2 velocity = new Vector2(-1f, -1f) * Main.rand.NextFloat(1f, 1.7f);
+                        velocity.X *= Main.rand.NextFloat(0.5f, 3f);
+
+                        if (Main.rand.NextBool(3))
+                            velocity *= 1.5f;
+                        Vector2 scale = new Vector2(0.25f, 0.4f) * 0.65f;
+                        int time = 110 + Main.rand.Next(70);
+                        Color color = Color.Lerp(Color.Red * 0.65f, Color.Purple, Main.rand.NextFloat(0.75f));
+                        Vector2 randParticlePos = Main.rand.NextVector2FromRectangle(new Rectangle((int)pos.X, (int)pos.Y, 32, 32));
+                        ParticleManager.AddParticle(new Blood(randParticlePos, velocity, time, Color.Black * 0.65f, scale, velocity.ToRotation(), false));
+                        ParticleManager.AddParticle(new Blood(randParticlePos, velocity, time, color, scale, velocity.ToRotation(), true));
+                    }
+                    Vector2 goreVel = new Vector2(Main.rand.NextFloat(-0.8f, 0.8f), Main.rand.NextFloat(-0.6f, 0.2f));
+                    goreVel += Vector2.UnitX * -1.5f;
+                    if (goreVel.X < 0)
+                        goreVel.X *= 3.5f;
+                    Gore.NewGore(NPC.GetSource_Death(), pos, goreVel, Main.rand.Next(140, 143));
+                }
             }
         }
         public override void ModifyIncomingHit(ref NPC.HitModifiers modifiers)
@@ -743,18 +911,39 @@ namespace TerRoguelike.NPCs.Enemy.Boss
         }
         public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
         {
+            if (deadTime > 0)
+            {
+                if (deathGodRays.Any())
+                {
+                    StartAdditiveSpritebatch();
+                    for (int i = 0; i < deathGodRays.Count; i++)
+                    {
+                        GodRay ray = deathGodRays[i];
+                        float rotation = ray.rotation;
+                        Vector2 rayScale = ray.scale;
+                        int time = ray.time;
+                        float opacity = MathHelper.Clamp(MathHelper.Lerp(1f, 0.5f, (deadTime - time) / 60f), 0.5f, 1f);
+                        Main.EntitySpriteDraw(godRayTex, ray.position - Main.screenPosition, null, Color.Red * opacity, rotation, new Vector2(0, godRayTex.Height * 0.5f), rayScale, SpriteEffects.None);
+                    }
+                    StartVanillaSpritebatch();
+                }
+            }
+
             Vector2 bodyDrawStart = NPC.Center + hitboxes[0].offset + new Vector2(-16, hitboxes[0].dimensions.Y * -0.5f) + Vector2.UnitY * 16 + Vector2.UnitX * -8;
+
             int eyeFrameCount = 2;
             int eyeFrameHeight = eyeTex.Height / eyeFrameCount;
             int currentEyeFrame = (int)(NPC.frameCounter % eyeFrameCount);
             Rectangle eyeFrame = new Rectangle(0, currentEyeFrame * eyeFrameHeight, eyeTex.Width, eyeFrameHeight - 2);
+
             int mouthFrameCount = 2;
             int mouthFrameHeight = mouthTex.Height / mouthFrameCount;
             int currentMouthFrame = (int)(mouthFrameCounter % eyeFrameCount);
             Rectangle mouthFrame = new Rectangle(0, currentMouthFrame * mouthFrameHeight, mouthTex.Width, mouthFrameHeight - 2);
+
             SpriteEffects spriteEffects = NPC.spriteDirection > 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
             Color bodyDrawColor;
-            float offsetMagnitude = (float)Math.Sin(NPC.localAI[0] * 0.03f);
+            float offsetMagnitude = (float)Math.Sin(NPC.localAI[1] * 0.03f);
             int bodyFrameYOff = ((int)(NPC.frameCounter * 2) % 3 * (bodyTex.Height / 3)) + (int)(-10 * offsetMagnitude);
 
             List<StoredDraw> draws = [];
@@ -795,10 +984,21 @@ namespace TerRoguelike.NPCs.Enemy.Boss
 
                 StartVanillaSpritebatch();
             }
+            Vector2 drawOff = -Main.screenPosition;
+            if (NPC.localAI[0] < -30)
+            {
+                float completion = ((-NPC.localAI[0] - 30f) / cutsceneDuration);
+                completion = MathHelper.SmoothStep(0, 1, completion);
+                drawOff += new Vector2(160 * completion, 0);
+            }
             for (int i = 0; i < draws.Count; i++)
             {
                 var draw = draws[i];
-                draw.Draw(-Main.screenPosition);
+                draw.Draw(drawOff);
+            }
+            if (NPC.localAI[0] < 0)
+            {
+                Main.EntitySpriteDraw(squareTex, NPC.Center + hitboxes[1].offset + new Vector2(284, 0) - Main.screenPosition, null, Color.Black, 0, squareTex.Size() * 0.5f, hitboxes[0].dimensions.ToVector2() * new Vector2(0.6f, 0.25f), SpriteEffects.None);
             }
 
             if (false)
