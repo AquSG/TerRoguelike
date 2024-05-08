2f8e9d8a237215da3bbcd2314d3c6d7e93341cc8
AquaSG <macvine9@gmail.com>
2024-05-08 17:19:51 -0400

    dungeon enemy layouts

--- Rooms/Dungeon/DungeonEnemyRoom2Down.cs
+++ Rooms/Dungeon/DungeonEnemyRoom2Down.cs
@@ -8,11 +8,14 @@ using TerRoguelike.Systems;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
+using static TerRoguelike.Managers.NPCManager;
+using static TerRoguelike.Schematics.SchematicManager;
 
 namespace TerRoguelike.Rooms
 {
     public class DungeonEnemyRoom2Down : Room
     {
+        public override int AssociatedFloor => FloorDict["Dungeon"];
         public override string Key => "DungeonEnemyRoom2Down";
         public override string Filename => "Schematics/RoomSchematics/DungeonEnemyRoom2Down.csch";
         public override bool CanExitRight => true;
@@ -20,7 +23,12 @@ namespace TerRoguelike.Rooms
         public override void InitializeRoom()
         {
             base.InitializeRoom();
-            AddRoomNPC(new Vector2(RoomDimensions.X * 8f, RoomDimensions.Y * 8f), NPCID.DiabolistRed, 60, 120, 0.45f);
+            AddRoomNPC(MakeEnemySpawnPos(TopLeft, 11, 14), ChooseEnemy(AssociatedFloor, 2), 60, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(TopRight, -24, 13), ChooseEnemy(AssociatedFloor, 2), 60, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(BottomLeft, 20, -17), ChooseEnemy(AssociatedFloor, 0), 60, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(Bottom, 0, -4), ChooseEnemy(AssociatedFloor, 1), 60, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(Left, 10, -1), ChooseEnemy(AssociatedFloor, 1), 60, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(Center, 5, -5), ChooseEnemy(AssociatedFloor, 0), 60, 120, 0.45f, 0);
         }
     }
 }
