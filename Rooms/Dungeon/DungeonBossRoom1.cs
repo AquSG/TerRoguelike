ac55f09802251cb6f70d73332d1c5bb46b3f23a6
AquaSG <macvine9@gmail.com>
2024-05-08 12:26:50 -0400

    spawn animation

--- Rooms/Dungeon/DungeonBossRoom1.cs
+++ Rooms/Dungeon/DungeonBossRoom1.cs
@@ -9,14 +9,18 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using static TerRoguelike.Schematics.SchematicManager;
+using static TerRoguelike.Utilities.TerRoguelikeUtils;
 using Terraria.ModLoader;
 using TerRoguelike.NPCs.Enemy.Boss;
 using TerRoguelike.World;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
 
 namespace TerRoguelike.Rooms
 {
     public class DungeonBossRoom1 : Room
     {
+        public Texture2D skeletronTex = null;
         public override int AssociatedFloor => FloorDict["Dungeon"];
         public override string Key => "DungeonBossRoom1";
         public override string Filename => "Schematics/RoomSchematics/DungeonBossRoom1.csch";
@@ -33,6 +37,21 @@ namespace TerRoguelike.Rooms
             if (bossSpawnPos == Vector2.Zero)
                 bossSpawnPos = new Vector2(RoomDimensions.X * 8f, RoomDimensions.Y * 8f);
             base.Update();
+        }
+        public override void PostDrawTilesRoom()
+        {
+            base.PostDrawTilesRoom();
+            if (initialized)
+                return;
+
+            int npcType = ModContent.NPCType<Skeletron>();
+            if (skeletronTex == null)
+                skeletronTex = TextureAssets.Npc[npcType].Value;
+            Vector2 drawPos = bossSpawnPos + RoomPosition16;
+            drawPos = TileCollidePositionInLine(drawPos, drawPos + new Vector2(0, 1000));
+            drawPos += new Vector2(0, -40);
+            Rectangle frame = new Rectangle(0, 0, skeletronTex.Width, (skeletronTex.Height / Main.npcFrameCount[npcType]) - 2);
+            Main.EntitySpriteDraw(skeletronTex, drawPos - Main.screenPosition, frame, Lighting.GetColor(drawPos.ToTileCoordinates()), Skeletron.spawnRotation, frame.Size() * 0.5f, 1f, SpriteEffects.None);
         }
     }
 }
