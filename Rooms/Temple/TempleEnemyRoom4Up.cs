be44b61fc8d707910eb6d5d143bac2c8d5d08766
AquaSG <macvine9@gmail.com>
2024-05-14 18:01:42 -0400

    temple enemy room layouts

--- Rooms/Temple/TempleEnemyRoom4Up.cs
+++ Rooms/Temple/TempleEnemyRoom4Up.cs
@@ -8,11 +8,14 @@ using TerRoguelike.Systems;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
+using static TerRoguelike.Managers.NPCManager;
+using static TerRoguelike.Schematics.SchematicManager;
 
 namespace TerRoguelike.Rooms
 {
     public class TempleEnemyRoom4Up : Room
     {
+        public override int AssociatedFloor => FloorDict["Temple"];
         public override string Key => "TempleEnemyRoom4Up";
         public override string Filename => "Schematics/RoomSchematics/TempleEnemyRoom4Up.csch";
         public override bool CanExitRight => true;
@@ -20,7 +23,12 @@ namespace TerRoguelike.Rooms
         public override void InitializeRoom()
         {
             base.InitializeRoom();
-            AddRoomNPC(new Vector2(RoomDimensions.X * 8f, RoomDimensions.Y * 8f), NPCID.FlyingSnake, 60, 120, 0.45f);
+            AddRoomNPC(MakeEnemySpawnPos(TopLeft, 10, 14), ChooseEnemy(AssociatedFloor, 2), 60, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(TopRight, -13, 23), ChooseEnemy(AssociatedFloor, 0), 60, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(Center, 0, 4, 0), ChooseEnemy(AssociatedFloor, 1), 60, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(Top, 0, 9, 0), ChooseEnemy(AssociatedFloor, 1), 60, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(BottomLeft, 7, -7), ChooseEnemy(AssociatedFloor, 2), 60, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(BottomRight, -5, -7), ChooseEnemy(AssociatedFloor, 0), 60, 120, 0.45f, 0);
         }
     }
 }
