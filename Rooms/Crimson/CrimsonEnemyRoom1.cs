b2e5437acfc731fbbaf446f2934132642e9e5253
AquaSG <macvine9@gmail.com>
2024-06-05 23:24:36 -0400

    edits

--- Rooms/Crimson/CrimsonEnemyRoom1.cs
+++ Rooms/Crimson/CrimsonEnemyRoom1.cs
@@ -10,6 +10,8 @@ using Terraria;
 using Terraria.ID;
 using static TerRoguelike.Managers.NPCManager;
 using static TerRoguelike.Schematics.SchematicManager;
+using Terraria.ModLoader;
+using TerRoguelike.NPCs.Enemy;
 
 namespace TerRoguelike.Rooms
 {
@@ -29,7 +31,7 @@ namespace TerRoguelike.Rooms
             AddRoomNPC(MakeEnemySpawnPos(Bottom, -4, -4), ChooseEnemy(AssociatedFloor, 0), 180, 120, 0.45f, 0);
 
             AddRoomNPC(MakeEnemySpawnPos(BottomRight, -3, -3), ChooseEnemy(AssociatedFloor, 0), 60, 120, 0.45f, 1);
-            AddRoomNPC(MakeEnemySpawnPos(BottomLeft, 4, -3), ChooseEnemy(AssociatedFloor, 2), 60, 120, 0.45f, 1);
+            AddRoomNPC(MakeEnemySpawnPos(BottomLeft, 4, -3), ModContent.NPCType<Crimator>(), 60, 120, 0.45f, 1);
         }
     }
 }
