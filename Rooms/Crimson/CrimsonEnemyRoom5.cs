0bd0af37fe37a33ae4825e633e1307a52c2d8ab3
AquaSG <macvine9@gmail.com>
2024-06-09 13:18:12 -0400

    edits

--- Rooms/Crimson/CrimsonEnemyRoom5.cs
+++ Rooms/Crimson/CrimsonEnemyRoom5.cs
@@ -10,6 +10,8 @@ using Terraria;
 using Terraria.ID;
 using static TerRoguelike.Managers.NPCManager;
 using static TerRoguelike.Schematics.SchematicManager;
+using Terraria.ModLoader;
+using TerRoguelike.NPCs.Enemy;
 
 namespace TerRoguelike.Rooms
 {
@@ -25,9 +27,9 @@ namespace TerRoguelike.Rooms
         {
             base.InitializeRoom();
             AddRoomNPC(MakeEnemySpawnPos(Top, 0, 3), ChooseEnemy(AssociatedFloor, 1), 60, 120, 0.45f, 0);
-            AddRoomNPC(MakeEnemySpawnPos(Right, -4, 0), ChooseEnemy(AssociatedFloor, 2), 60, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(Right, -4, 0), ModContent.NPCType<Crimator>(), 60, 120, 0.45f, 0);
             AddRoomNPC(MakeEnemySpawnPos(BottomRight, -3, -3), ChooseEnemy(AssociatedFloor, 0), 60, 120, 0.45f, 0);
-            AddRoomNPC(MakeEnemySpawnPos(BottomLeft, 3, -3), ChooseEnemy(AssociatedFloor, 0), 180, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(BottomLeft, 3, -3), ChooseEnemy(AssociatedFloor, 0), 240, 120, 0.45f, 0);
 
             AddRoomNPC(MakeEnemySpawnPos(Top, 0, 3), ChooseEnemy(AssociatedFloor, 1), 60, 120, 0.45f, 1);
             AddRoomNPC(MakeEnemySpawnPos(Left, 6, 0), ChooseEnemy(AssociatedFloor, 2), 60, 120, 0.45f, 1);
