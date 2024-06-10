209738851c1c192cbb63ee24bd081452636e6348
AquaSG <macvine9@gmail.com>
2024-06-10 12:26:04 -0400

    new rooms and room edits

---
+++ Rooms/Base/BaseEnemyRoom9.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerRoguelike.Managers;
+using TerRoguelike.Systems;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using static TerRoguelike.Managers.NPCManager;
+using static TerRoguelike.Schematics.SchematicManager;
+
+namespace TerRoguelike.Rooms
+{
+    public class BaseEnemyRoom9 : Room
+    {
+        public override int AssociatedFloor => FloorDict["Base"];
+        public override string Key => "BaseEnemyRoom9";
+        public override string Filename => "Schematics/RoomSchematics/BaseEnemyRoom9.csch";
+        public override bool CanExitRight => true;
+        public override bool CanExitDown => true;
+        public override bool CanExitUp => true;
+        public override void InitializeRoom()
+        {
+            base.InitializeRoom();
+            AddRoomNPC(MakeEnemySpawnPos(BottomRight, -5, -2), ChooseEnemy(AssociatedFloor, 2), 60, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(Center, 3, 3), ChooseEnemy(AssociatedFloor, 1), 60, 120, 0.45f, 0);
+            AddRoomNPC(MakeEnemySpawnPos(Center, -4, -4), ChooseEnemy(AssociatedFloor, 0), 60, 120, 0.45f, 0);
+
+            AddRoomNPC(MakeEnemySpawnPos(BottomLeft, 5, -2), ChooseEnemy(AssociatedFloor, 2), 60, 120, 0.45f, 1);
+            AddRoomNPC(MakeEnemySpawnPos(Center, -3, 3), ChooseEnemy(AssociatedFloor, 1), 60, 120, 0.45f, 1);
+            AddRoomNPC(MakeEnemySpawnPos(Left, 3, -1), ChooseEnemy(AssociatedFloor, 2), 60, 120, 0.45f, 1);
+        }
+    }
+}
